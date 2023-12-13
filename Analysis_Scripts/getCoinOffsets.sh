9942d3fb782fcdb3b9a8497089e887e69e0c012f
heinricn <heinricn@uregina.ca>
2023-12-13 14:40:39 -0600

    added quick script for coin offsets

---
+++ Analysis_Scripts/getCoinOffsets.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#UNNUMBER=$1
+### Check you've provided the first argument  
+RunList=$1
+if [[ -z "$1" ]]; then
+    echo "I need a run list process!"
+    echo "Please provide a run list as input"
+    exit 2
+fi
+
+REPLAYPATH="/home/heinricn/work/JLab/hallc_replay_lt/"
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+                echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+                echo "Run number read from file: $line"
+                echo ""
+                ##Run number#
+                RUNNUMBER=$line
+                
+                cd "$REPLAYPATH/CALIBRATION/"
+                root -l -b -q "getCoinOffset.C(\"$REPLAYPATH/ROOTfiles/Analysis/PionLT/Pion_coin_replay_production_$RUNNUMBER_-1.root\",$RUNNUMBER)"
+
+done < "$RunList"
